67644977497a5a0a8222654a0ad4da88645dde34
EJLarcs <ejlarcombe@gmail.com>
2015-08-04 14:04:56 -0400

    barking

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog
+  # attr_accessor :name
+  # attr_reader :bark
+
+  # def initialize(name, bark)
+  #   @name = name
+  #   @bark = bark
+  # end
+
+   def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
+
+
